3f7f143ced21f4086f7992120e7229789c7ff529
Noel Emmanuel Roodly <emmanuel.noel@wyzdev.net>
2021-05-30 15:08:34 -0400

    factures

---
+++ db/Invoice.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noel Emmanuel Roodly
+ * Date: 5/30/2021
+ * Time: 11:49 AM
+ */
+
+class Invoice
+{
+
+}
---
+++ db/Pricing.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noel Emmanuel Roodly
+ * Date: 5/30/2021
+ * Time: 12:29 PM
+ */
+
+class Pricing
+{
+
+}
