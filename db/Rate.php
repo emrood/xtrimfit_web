5619362ccf422f837999d86932c13e356149019e
Noel Emmanuel Roodly <emmanuel.noel@wyzdev.net>
2021-06-04 02:36:10 -0400

    L

---
+++ db/Rate.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noel Emmanuel Roodly
+ * Date: 6/3/2021
+ * Time: 8:55 PM
+ */
+
+class Rate
+{
+
+}
---
+++ db/Setting.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Noel Emmanuel Roodly
+ * Date: 6/4/2021
+ * Time: 12:54 AM
+ */
+
+class Setting
+{
+
+}
